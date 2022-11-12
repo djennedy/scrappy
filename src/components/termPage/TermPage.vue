dfb9c0bd1bf67606df09091c69e1b548e02a1454
Sarah Vo <tsvo@sfu.ca>
2022-11-12 10:10:03 -0800

    Finish term page card component

---
+++ src/components/termPage/TermPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="w-full h-[100px]"></header>
+    <div class="flex flex-col items-center pt-8 gap-2">
+      <TermPageCard
+          v-for="course in exampleData"
+          v-bind="course"
+      />
+    </div>
+</template>
+
+<script>
+import TermPageCard from "./components/TermPageCard.vue";
+export default {
+  name: "TermPage",
+  components: {TermPageCard},
+  data(){
+    return{
+      exampleData: [
+          {courseNumber: "CMPT 272", courseName: "Web I - Client-side Development", instructor: "Bobby Chan", campusLocation:"Burnaby",wqbDesignation:"Breadth", creditsNum:"3"},
+          {courseNumber: "CMPT 272", courseName: "Web I - Client-side Development", instructor: "Bobby Chan", campusLocation:"Burnaby",wqbDesignation:"Breadth", creditsNum:"3"},
+          {courseNumber: "CMPT 272", courseName: "Web I - Client-side Development", instructor: "Bobby Chan", campusLocation:"Burnaby",wqbDesignation:"Breadth", creditsNum:"3"},
+          {courseNumber: "CMPT 272", courseName: "Web I - Client-side Development", instructor: "Bobby Chan", campusLocation:"Burnaby",wqbDesignation:"Breadth", creditsNum:"3"},
+          {courseNumber: "CMPT 272", courseName: "Web I - Client-side Development", instructor: "Bobby Chan", campusLocation:"Burnaby",wqbDesignation:"Breadth", creditsNum:"3"},
+      ],
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/termPage/components/TermPageCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <!--  Term Page Course Card-->
+  <div class="
+  flex flex-row justify-between border-[1px] border-[#E4E4E4]
+   rounded-[8px] px-4 py-6 w-[80%]">
+    <div class="flex flex-row justify-between items-center flex-1">
+      <p class="font-bold text-xl">{{ courseNumber }}</p>
+      <p class="font-semibold">{{ courseName }}</p>
+    </div>
+    <div class="flex flex-row justify-center items-center gap-6 flex-1">
+      <p>{{ instructor }}</p>
+      <p>{{ campusLocation }}</p>
+      <p>{{ wqbDesignation }}</p>
+      <p>{{ creditsNum }}</p>
+    </div>
+    <button class="
+    p-1.5 border-2 border-blue-500 rounded-[4px] bg-blue-500 text-white font-bold">
+      Details
+    </button>
+  </div>
+</template>
+
+<script>
+import "../../../assets/main.css";
+export default {
+  name: "termPageCard",
+  props: {
+    courseNumber: String,
+    courseName: String,
+    instructor: String,
+    campusLocation: String,
+    wqbDesignation: String,
+    creditsNum: Number,
+  },
+};
+</script>
+
+<style scoped></style>
