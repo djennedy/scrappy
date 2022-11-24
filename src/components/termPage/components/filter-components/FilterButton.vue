cb50ebe220250c28caca38bb76c3f4de0d3b07eb
Sarah Vo <tsvo@sfu.ca>
2022-11-23 18:50:52 -0800

    Styled filter, but using headless so many need to remove it and DIY

--- src/components/termPage/components/filter-components/FilterButton.vue
+++ src/components/termPage/components/filter-components/FilterButton.vue
@@ -1,11 +1,22 @@
 <template>
-  <Menu>
-    <MenuButton>{{this.menuTitle}}</MenuButton>
-    <MenuItems v-for="option in this.options" :key="option.href">
-      <MenuItem v-on:click="optionClicked(option)">
-        <a href={{option.href}}>{{ option.label }}</a>
-      </MenuItem>
-    </MenuItems>
+  <Menu as="div" class="relative inline-block text-left">
+    <MenuButton class="border-2 rounded-md p-2 font-medium">
+      {{this.menuTitle}}
+    </MenuButton>
+    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+      <MenuItems as="div" class="
+      absolute left-0 z-10 mt-2 w-56 origin-top-right
+      rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div class="flex flex-col">
+            <MenuItem v-for="option in this.options" :key="option.href" v-on:click="optionClicked(option)"
+                      @mouseover="e => e.target.classList.toggle('bg-gray-300')"
+                      @mouseleave="e => e.target.classList.toggle('bg-gray-300')"
+                      class="p-4 transition-colors">
+              <a href="#">{{ option.label }}</a>
+            </MenuItem>
+        </div>
+      </MenuItems>
+    </transition>
   </Menu>
 </template>
 
