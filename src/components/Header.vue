ee89d3f96d11eee67988a865c92f7763ec3702b3
Alex Lazcano <alazcano2000@gmail.com>
2022-11-24 16:07:39 -0800

    Added header component and worked on layout, changed HomeView to match mockups

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <div class="header">
+        <div class="text-black text-lg font-bold">
+            Scrappy
+        </div>
+        <div class="dropdown-btn bg-slate-200">
+            <img class="mr-5 rounded-full" src="https://via.placeholder.com/30" alt="user image"/>
+            <p class="text-black font-semibold">
+                Alexis Lazcano
+            </p>
+        </div>
+    </div>
+
+  </header>
+ 
+</template>
+
+<style scoped>
+    header {
+        background-color: white;
+        height: 75px;
+        display: flex;
+        justify-content: center;
+        top: 0;
+        z-index: 1;
+        position: fixed;
+        width: 100%;
+    }
+    .header {
+        height: 100%;
+        width: 100%;
+        max-width: 1000px;
+        background-color: #fff;
+        display: flex;
+        justify-content: space-between;
+        padding: 0 1rem;
+        align-items: center;
+        
+    }
+    .dropdown-btn {
+        display: flex;
+        align-items: center;
+        padding: 8px 10px;
+        border-radius: 5px;
+        
+    }
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-5xl mb-16">Welcome to Scrappy</h1>
+    <h3 class="text-xl mb-10">A SFU planner where you can plan your courses <br/> for future semesters</h3>
+    <h2>Search for a Specific Course</h2>
+    <div class="search">
+      <input type="text" placeholder="Search for a course" v-model="search" />
+      <button @click="searchCourse">Search</button>
+    </div>  
+    <p>
+      or
+    </p>
+    <a class="underline text-black">Search courses per term</a>
   </main>
 </template>
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 10rem;
+  
+}
+
+.search {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 1rem;
+}
+h1,h2,h3,h4,h5,h6 {
+  text-align: center;
+}
+</style>
