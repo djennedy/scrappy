e75e8e635e3c58316ad617d00bb3daba6ddc2c05
Justin Ang <jrang@sfu.ca>
2022-12-26 06:57:18 +0700

    trying out instructors card

---
+++ src/components/coursePage/CoursePage.vue
@@ -0,0 +1,10 @@
+<script setup>
+import InstructorsCard from "./components/InstructorsCard.vue";
+</script>
+
+
+<template>
+  <div class="flex flex-col items-center">
+    <InstructorsCard />
+  </div>
+</template>
---
+++ src/components/coursePage/components/InstructorsCard.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    instructor: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <h2>D200</h2>
+    <p>Hazra Imran</p>
+    <p>Mo 12:30PM - 1:20PM</p>
+    <p>WMC 3520, Burnaby</p>
+    <p>Th 12:30PM - 2:20PM</p>
+    <p>RCB IMAGTH, Burnaby</p>
+  </div>
+</template>
