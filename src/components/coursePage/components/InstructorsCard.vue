0385c2f75293e4744502cabe5a62772d413edd67
Justin Ang <jrang@sfu.ca>
2022-12-28 08:16:21 +0700

    added iconify icon

--- src/components/coursePage/components/InstructorsCard.vue
+++ src/components/coursePage/components/InstructorsCard.vue
@@ -1,4 +1,5 @@
 <script>
+import "iconify-icon";
 export default {
   props: {
     instructor: Object,
@@ -7,8 +8,10 @@ export default {
 </script>
 
 <template>
-  <div class="flex flex-col">
-    <h2>D200</h2>
+  <div
+    class="flex flex-col border-[1px] border-[#000000] border-solid rounded-[10px] px-[9.5px] py-[10.64px]"
+  >
+    <h2>D200 <iconify-icon icon="bi:question-circle" /></h2>
     <p>Hazra Imran</p>
     <p>Mo 12:30PM - 1:20PM</p>
     <p>WMC 3520, Burnaby</p>
