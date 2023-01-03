0f09c5860eb10529db48c3af690a8098365c70bb
Justin Ang <jrang@sfu.ca>
2023-01-03 22:24:05 +0700

    finished props

--- src/components/coursePage/CoursePage.vue
+++ src/components/coursePage/CoursePage.vue
@@ -1,10 +1,70 @@
-<script setup>
+<script>
 import InstructorsCard from "./components/InstructorsCard.vue";
+
+export default {
+  components: {
+    InstructorsCard,
+  },
+  data() {
+    return {
+      instructors: [
+        {
+          id: 1,
+          section: "D200",
+          instructor: "Hazra Imran",
+          timeLocations: [
+            "Mo 12:30PM - 1:20PM WMC 3520, Burnaby",
+            "Th 12:30PM - 2:20PM RCB IMAGTH, Burnaby",
+          ],
+        },
+        {
+          id: 2,
+          section: "D100",
+          instructor: "Harinder Khangura",
+          timeLocations: [
+            "We 9:30AM – 10:20AM SRYE 3016, Surrey",
+            "Fr 8:30AM – 10:20AM SRYE 3016, Surrey",
+          ],
+        },
+        {
+          id: 3,
+          section: "D200",
+          instructor: "Hazra Imran",
+          timeLocations: [
+            "Mo 12:30PM - 1:20PM WMC 3520, Burnaby",
+            "Th 12:30PM - 2:20PM RCB IMAGTH, Burnaby",
+          ],
+        },
+        {
+          id: 4,
+          section: "D100",
+          instructor: "Harinder Khangura",
+          timeLocations: [
+            "We 9:30AM – 10:20AM SRYE 3016, Surrey",
+            "Fr 8:30AM – 10:20AM SRYE 3016, Surrey",
+          ],
+        },
+        {
+          id: 5,
+          section: "D200",
+          instructor: "Hazra Imran",
+          timeLocations: [
+            "Mo 12:30PM - 1:20PM WMC 3520, Burnaby",
+            "Th 12:30PM - 2:20PM RCB IMAGTH, Burnaby",
+          ],
+        },
+      ],
+    };
+  },
+};
 </script>
 
-
 <template>
-  <div class="flex flex-col items-center">
-    <InstructorsCard />
+  <div class="flex flex-row flex-wrap justify-start gap-4">
+    <InstructorsCard
+      v-for="instructor in instructors"
+      :key="instructor.id"
+      :instructor="instructor"
+    />
   </div>
 </template>
---
+++ src/components/coursePage/components/TimeLocation.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  props: {
+    timeLocation: String,
+  },
+};
+</script>
+
+<template>
+  <p class="text-xs">{{ timeLocation }}</p>
+</template>
