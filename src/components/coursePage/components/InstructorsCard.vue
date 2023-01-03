e13482e30284b04622dd7ba4c07a11d1e992efb5
Justin Ang <jrang@sfu.ca>
2023-01-03 18:20:41 +0700

    added styling

--- src/components/coursePage/components/InstructorsCard.vue
+++ src/components/coursePage/components/InstructorsCard.vue
@@ -9,13 +9,14 @@ export default {
 
 <template>
   <div
-    class="flex flex-col border-[1px] border-[#000000] border-solid rounded-[10px] px-[9.5px] py-[10.64px]"
+    class="box-border max-w-[190px] max-h-[214px] flex flex-col border-[1px] border-[#000000] border-solid rounded-[10px] pl-2.5 pt-3 pb-5 font-normal"
   >
-    <h2>D200 <iconify-icon icon="bi:question-circle" /></h2>
-    <p>Hazra Imran</p>
-    <p>Mo 12:30PM - 1:20PM</p>
-    <p>WMC 3520, Burnaby</p>
-    <p>Th 12:30PM - 2:20PM</p>
-    <p>RCB IMAGTH, Burnaby</p>
+    <div class="flex items-center space-x-2.5 text-base mb-2">
+      <p>D200</p>
+      <iconify-icon icon="bi:question-circle" />
+    </div>
+    <p class="font-medium text-sm mt-0 mb-4">Hazra Imran</p>
+    <p class="text-xs mb-4">Mo 12:30PM - 1:20PM WMC 3520, Burnaby</p>
+    <p class="text-xs">Th 12:30PM - 2:20PM RCB IMAGTH, Burnaby</p>
   </div>
 </template>
