7b3e6169775347a9951ac0fbdfb4774c9fa45a72
Justin Ang <jrang@sfu.ca>
2023-01-03 22:24:05 +0700

    finished props

--- src/components/coursePage/components/InstructorsCard.vue
+++ src/components/coursePage/components/InstructorsCard.vue
@@ -1,8 +1,13 @@
 <script>
 import "iconify-icon";
+import TimeLocation from "./TimeLocation.vue";
+
 export default {
   props: {
     instructor: Object,
+  },
+  components: {
+    TimeLocation,
   },
 };
 </script>
@@ -12,11 +17,18 @@ export default {
     class="box-border max-w-[190px] max-h-[214px] flex flex-col border-[1px] border-[#000000] border-solid rounded-[10px] pl-2.5 pt-3 pb-5 font-normal"
   >
     <div class="flex items-center space-x-2.5 text-base mb-2">
-      <p>D200</p>
+      <p>{{ instructor.section }}</p>
       <iconify-icon icon="bi:question-circle" />
     </div>
-    <p class="font-medium text-sm mt-0 mb-4">Hazra Imran</p>
-    <p class="text-xs mb-4">Mo 12:30PM - 1:20PM WMC 3520, Burnaby</p>
-    <p class="text-xs">Th 12:30PM - 2:20PM RCB IMAGTH, Burnaby</p>
+    <p class="font-medium text-sm mt-0 mb-4">{{ instructor.instructor }}</p>
+    <div class="space-y-4">
+      <!-- <p class="text-xs">Mo 12:30PM - 1:20PM WMC 3520, Burnaby</p>
+      <p class="text-xs">Th 12:30PM - 2:20PM RCB IMAGTH, Burnaby</p> -->
+      <TimeLocation
+        v-for="timeLocation in instructor.timeLocations"
+        :key="timeLocation.id"
+        :timeLocation="timeLocation"
+      />
+    </div>
   </div>
 </template>
