e15e3672e878af9f8b7f478a1eebe6fe07c936fc
Justin Ang <jrang@sfu.ca>
2023-01-12 17:46:08 +0700

    added course info

---
+++ src/components/coursePage/components/CourseInfo.vue
@@ -0,0 +1,12 @@
+<script></script>
+
+<template>
+  <div class="max-w-[676px] max-height-[128px]">
+    <h2 class="font-normal text-[#B0001A] text-[18px] leading-[115%] mb-0">
+      Spring 2023 - CMPT120
+    </h2>
+    <h1 class="font-semibold text-[32px] leading-[39px] mt-[9.34px] mb-[12px]">
+      INTRODUCTION TO COMPUTING SCIENCE AND PROGRAMMING I
+    </h1>
+  </div>
+</template>
