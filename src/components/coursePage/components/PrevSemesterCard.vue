819f14b368f9b6de8c0a9aef97db8278f90e6e4d
Justin Ang <jrang@sfu.ca>
2023-01-13 09:24:35 +0700

    re optimize for wide screen display

--- src/components/coursePage/components/PrevSemesterCard.vue
+++ src/components/coursePage/components/PrevSemesterCard.vue
@@ -13,11 +13,9 @@ export default {
 
 <template>
   <div
-    class="box-border max-w-[189px] max-h-[142px] flex flex-col border-[1px] border-[#000000] border-solid rounded-[10px] font-normal"
+    class="box-border w-[207px] h-[142px] flex flex-col border-[1px] border-[#000000] border-solid rounded-[10px] font-normal"
   >
-    <div
-      class="max-w-[189px] max-h-[35px] bg-[#616161] flex flex-col rounded-t-[10px]"
-    >
+    <div class="w-[207px] h-[35px] bg-[#616161] flex flex-col rounded-t-[10px]">
       <p
         class="font-semibold text-[22px] leading-[27px] text-white w-[169px] max-h-[25px] ml-[11px] my-[5px]"
       >
