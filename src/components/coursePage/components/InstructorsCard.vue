d5d5e1e7484b2261335036c63130a49269d5f51a
Justin Ang <jrang@sfu.ca>
2023-01-13 09:15:20 +0700

    finished course offering subsection and prev semester subsection p1. 1

--- src/components/coursePage/components/InstructorsCard.vue
+++ src/components/coursePage/components/InstructorsCard.vue
@@ -14,13 +14,13 @@ export default {
 
 <template>
   <div
-    class="box-border max-w-[190px] max-h-[214px] flex flex-col border-[1px] border-[#000000] border-solid rounded-[10px] pl-2.5 pt-3 pb-5 font-normal"
+    class="box-border w-[190px] h-[214px] flex flex-col border-[1px] border-[#000000] border-solid rounded-[10px] pl-2.5 pt-3 pb-5 font-normal"
   >
     <div class="flex items-center space-x-2.5 text-[20px] leading-[24px] mb-2">
       <p>{{ instructor.section }}</p>
       <iconify-icon icon="bi:question-circle" />
     </div>
-    <p class="font-medium text-[18px] leading-[22px] mt-0 mb-4">
+    <p class="font-medium text-[18px] leading-[22px] mt-0 mb-6">
       {{ instructor.instructor }}
     </p>
     <div class="space-y-4">
