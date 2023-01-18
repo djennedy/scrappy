f2e00a5cdbc9ada2783bae90d131ec33295f2b25
AlexLazcano <47799660+AlexLazcano@users.noreply.github.com>
2023-01-17 20:07:23 -0800

    Adding Vuetify 3 to Project (#8)
    * Reorganized header containers, created SearchBar.vue file and started implementing Search bar
    
    * Put page width into a css variable
    
    * Redoing commit with npm for vuetify
    
    * fixed rebase, removed extra bracket

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,29 @@
 <script setup>
+import SearchBar from "../components/SearchBar.vue";
 </script>
 
 <template>
   <main>
-    <h1 class="text-5xl mb-16">Welcome to Scrappy</h1>
-    <h3 class="text-xl mb-10">A SFU planner where you can plan your courses <br/> for future semesters</h3>
-    <h2>Search for a Specific Course</h2>
-    <div class="search">
-      <input type="text" placeholder="Search for a course" v-model="search" />
-      <button @click="searchCourse">Search</button>
-    </div>  
-    <p>
-      or
-    </p>
-    <a class="underline text-black">Search courses per term</a>
+    <h1 class="text-5xl mb-16">Search for a SFU course</h1>
+    <SearchBar />
+    <a class="mt-6 text-black underline">Search courses by term</a>
+    <v-container class="info rounded-lg mt-20">
+      <v-row >
+        <v-col sm="6" cols="12">
+          <h2 class="text-lg font-bold">About Scrappy</h2>
+          <p>Scrappy is an information hub for SFU courses to help you make the most of your university courses.</p>
+        </v-col>
+        <v-col sm="6" cols="12">
+          <h2 class="text-lg font-bold">Don't know your degree requirements?</h2>
+          <v-btn variant="outlined" color="red" class="mt-7 degree-button" block>
+            <span class="font-bold">
+              Find your degree requirements
+            </span>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    
   </main>
 </template>
 <style scoped>
@@ -24,7 +34,9 @@ main {
   padding-top: 10rem;
   
 }
-
+degree-button {
+  border-width: 20px!important;
+}
 .search {
   display: flex;
   align-items: center;
@@ -34,4 +46,9 @@ main {
 h1,h2,h3,h4,h5,h6 {
   text-align: center;
 }
+.info {
+  background-color: white;
+  max-width: 800px;
+}
+
 </style>
