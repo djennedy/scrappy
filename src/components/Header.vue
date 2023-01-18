01693bc02b61fc31532aac697a4b373f767ac16f
AlexLazcano <47799660+AlexLazcano@users.noreply.github.com>
2023-01-17 19:40:07 -0800

    Header and Search Bar components (#3)
    * Added header component and worked on layout, changed HomeView to match mockups
    
    * Reorganized header containers, created SearchBar.vue file and started implementing Search bar
    
    * Changed searchbar to use input in search function
    
    * Put page width into a css variable
    
    * increased size of logo, added icons to svg file, changed icons on header to svg

---
+++ src/components/Header.vue
@@ -0,0 +1,107 @@
+<script>
+import SearchBar from "./SearchBar.vue";
+
+export default {
+  components: {
+    SearchBar,
+  },
+  data() {
+    return {
+      showSearchBar: true,
+      username: "Username",
+    };
+  },
+  methods: {
+    goHome() {
+      this.$router.push({ name: "home" });
+    },
+  }
+};
+</script>
+
+<template>
+  <header>
+    <div class="header">
+      <div class="header-container ">
+        <div class="scrappy text-black text-4xl font-bold cursor-pointer" @click="goHome">Scrappy</div>
+      </div>
+      <div class="header-container middle">
+        <SearchBar v-if="showSearchBar" />
+      </div>
+      <div class="header-container">
+        <div class="dropdown-btn">
+          <img
+            src="../assets/svg/account_circle.svg"
+            alt="dropdown icon"
+          />
+          <img
+            src="../assets/svg/arrow_dropdown.svg"
+            alt="user image"
+          />
+          
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.middle {
+  max-width: 675px;
+  width: 100%;
+}
+.header-container {
+  display: flex;
+
+  justify-content: center;
+}
+.header-container:first-child {
+  margin-right: auto;
+  justify-content: flex-start;
+  flex: 1;
+}
+.header-container:last-child {
+  flex: 1;
+  margin-left: auto;
+  justify-content: flex-end;
+}
+
+header {
+  background-color: white;
+  height: 75px;
+  display: flex;
+  justify-content: center;
+  top: 0;
+  z-index: 1;
+  position: fixed;
+  width: 100%;
+}
+
+.dropdown-btn {
+  display: flex;
+  justify-content: center;
+  padding: 0 5px;
+  border-radius: 5px;
+  height: 30px;
+  max-width: 120px;
+  font-size: 11px;
+  cursor: pointer;
+  align-items: center;
+}
+.dropdown-btn > img {
+  height: 30px;
+  width: auto;
+}
+.header {
+  height: 100%;
+  width: 100%;
+  max-width: var(--page-width);
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 1rem;
+  align-items: center;
+}
+
+
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  width: 100,
+  data() {
+    return {
+      search: "",
+    };
+  },
+  methods: {
+    searchText() {
+      console.log(this.search);
+      this.$router.push({ name: this.search });
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="search-bar">
+    <button class="search-btn mr-2">
+      <img src="https://via.placeholder.com/15" alt="search icon" />
+    </button>
+
+    <input
+      type="text"
+      placeholder="Search"
+      class="search-input"
+      v-model="search"
+      @input="$emit('input', search)"
+      @keyup.enter="searchText"
+    />
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  color: red;
+}
+div.search-bar {
+  border: 1px solid rgba(149, 149, 149, 1);
+  display: flex;
+  align-items: center;
+  padding: 8px 10px;
+  border-radius: 30px;
+  height: 31px;
+  max-width: 675px;
+  width: 100%;
+}
+input {
+  border: none;
+  outline: none;
+  font-size: 1rem;
+  padding: 0 10px;
+  color: black;
+  width: 100%;
+}
+
+input:focus {
+  border: none;
+  outline: none;
+}
+</style>
