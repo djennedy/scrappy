77426eac484983e6558ece723a23b88e0372a3e6
Justin Ang <jrang@sfu.ca>
2023-02-10 00:11:18 -0800

    on hover save button is darker

--- src/components/coursePage/components/SaveCourse.vue
+++ src/components/coursePage/components/SaveCourse.vue
@@ -2,7 +2,7 @@
 
 <template>
   <button
-    class="bg-[#D3001F] rounded-[8px] max-w-[253px] max-h-[48px] gap-[10px] py-[12px] px-[28px]"
+    class="bg-[#D3001F] rounded-[8px] max-w-[253px] max-h-[48px] gap-[10px] py-[12px] px-[28px] hover:bg-[#B0001A]"
   >
     <p class="text-white text-[18px] leading-[22px] font-semibold">
       Add to Saved Courses
