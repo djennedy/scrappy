6b9f4620646704350100983125cc3118328adfa5
dga48 <darrickgunawan@gmail.com>
2023-03-07 15:25:25 -0800

    only styling left

--- src/components/requirementsPage/components/DegreeRequirements.vue
+++ src/components/requirementsPage/components/DegreeRequirements.vue
@@ -10,7 +10,18 @@ export default {
 </script>
 
 <template>
-    <RequirementNumber number="1." />
+    <div>
+        <h1>Find your degree requirements</h1>
+        <h2>Please respond to all boxes</h2>
+        Choose your program
+        <RequirementNumber number="1." />
+        Choose your Major/Minor/Joint Major
+        <RequirementNumber number="2." />
+        Select the year you first enrolled in selected program
+        <RequirementNumber number="3." />
+        Select the term you first enrolled in selected program
+        <RequirementNumber number="4." />
+    </div>
 </template>
 
 <style scoped>
