b06a1c75739d60e0ab248520b3f6e959deb8367c
djennedy <darrenjennedy@gmail.com>
2023-03-23 19:16:24 -0700

    Added function to link to the area of study page

---
+++ src/components/functions/requirementsFunctions.js
@@ -0,0 +1,15 @@
+import { BASE_CALENDAR_URL } from "./commonFunctions";
+
+/**
+ * Function to get the link to SFU's area of study page.
+ * Not linking to the final page since it's not as trivially programmable
+ * @param {Number} year Number containing the user's year of enrollment // eg: 2019
+ * @param {String} term String containing the user's term of enrollment // eg: Fall
+ * @param {String} areaOfStudy String containing the user's current area or study  // eg: Computing Science
+ * @returns {String} URL of the Area of Study Page. User then go to their requirements page from there.
+ */
+const getAreaOfStudyPage = (year, term, areaOfStudy) => {
+    term = term.toLowerCase();
+    areaOfStudy = areaOfStudy.toLowerCase().replace(" ", "-");
+    return `${BASE_CALENDAR_URL}/${year}/${term}/areas-of-study/${areaOfStudy}.html`;
+}
