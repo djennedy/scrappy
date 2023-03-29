ac39399831861e7ebb9d2fdc287e903ade6f238d
djennedy <darrenjennedy@gmail.com>
2023-03-29 16:06:58 -0700

    red stuff

--- src/components/requirementsPage/components/GoButton.vue
+++ src/components/requirementsPage/components/GoButton.vue
@@ -1,10 +1,13 @@
 <script>
-import RequirementsButton from './RequirementsButton.vue'
-import { getAreaOfStudyPage } from '../../functions/requirementsFunctions';
+import { getAreaOfStudyPage } from "../../functions/requirementsFunctions";
 
 export default {
-  components: { RequirementsButton },
-  name: 'GoButton',
+  data() {
+    return {
+      
+    }
+  },
+  name: "GoButton",
   props: {
     program: String,
     year: String,
@@ -12,21 +15,55 @@ export default {
   },
   methods: {
     goToPage() {
-      if (this.program !== '' && this.year !== '' && this.semester !== '')
-      {
-        const pageURL = getAreaOfStudyPage(this.year, this.semester, this.program);
-        window.open(pageURL, '_blank');
+      if (this.program !== "" && this.year !== "" && this.semester !== "") {
+        const pageURL = getAreaOfStudyPage(
+          this.year,
+          this.semester,
+          this.program
+        );
+        window.open(pageURL, "_blank");
       }
     },
-},
-}
+  },
+  computed: {
+    buttonColor() {
+      if (this.program !== "" && this.year !== "" && this.semester !== "") {
+        return "bg-[#D3001F] text-white"
+      } else {
+        return "bg-gray-100 text-gray-400"
+      }
+    }
+  }
+};
 </script>
 
 <template>
   <div class="flex-container">
-    <RequirementsButton text="Go to your requirements" class="flex-items" @click="goToPage" />
+    <button
+      @click="goToPage"
+      class="flex-items px-4 py-2 rounded w-64 h-12 text-left relative text-xs"
+      :class="buttonColor"
+    >
+      Go to your requirements
+      <span class="absolute right-0 top-1/2 transform -translate-y-1/2">
+        <svg
+          class="fill-current h-4 w-4"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path
+            d="M6.293 14.293a1 1 0 0 1-1.414-1.414L8.586 10l-3.707-3.879a1 1 0 0 1 1.414-1.414l4 4a1 1 0 0 1 0 1.414l-4 4z"
+          />
+        </svg>
+      </span>
+    </button>
     <div class="flex-items">
-      <img src="../../../assets/svg/mountains.svg" alt="mountain" id="image" class="mountain">
+      <img
+        src="../../../assets/svg/mountains.svg"
+        alt="mountain"
+        id="image"
+        class="mountain"
+      />
     </div>
   </div>
 </template>
